63a87d0d8611f1f90133661386789a99efc8133d
Romualdo Krameck <romualdo.jr@gmail.com>
2021-09-30 20:51:04 -0300

    Add project files.

---
+++ JKM.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace JKM.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ JKM.Identidade.Api/Controllers/AuthController.cs
@@ -0,0 +1,61 @@
+﻿using JKM.Identidade.Api.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JKM.Identidade.Api.Controllers
+{
+    [Route("api/identidade")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("nova-conta")]
+        public async Task<ActionResult> Registrar(UsuarioRegistro  usuarioRegistro)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var user = new IdentityUser()
+            {
+                UserName = usuarioRegistro.Email,
+                Email = usuarioRegistro.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, usuarioRegistro.Senha);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+        public async Task<ActionResult> Login (UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var result = await _signInManager.PasswordSignInAsync(usuarioLogin.Email, usuarioLogin.Senha, false, true);
+
+            if (result.Succeeded)
+                return Ok();
+
+            return BadRequest();
+
+        }
+    }
+}
